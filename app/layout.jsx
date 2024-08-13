e13cb611e0af380808f95e596c6ae50ece2487c4
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-13 23:23:22 +0900

    Layout and HomePage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,14 @@
+import "@/assets/styles/globals.css";
 import React from "react";
 
-const Mainlayout = () => {
-  return;
-  <html lang="end">
-    <div>Mainlayout</div>;
-  </html>;
+const Mainlayout = ({ children }) => {
+  return (
+    <html lang="end">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 };
 
 export default Mainlayout;
