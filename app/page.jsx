5125e8b8b3e76f249963198ff8f7e74fd9efcd21
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-18 00:49:35 +0900

    Home Property Listings

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import Properties from "./properties/page";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <InfoBoxes />
-      <Properties />
+      <HomeProperties />
       <Hero />
     </>
   );
