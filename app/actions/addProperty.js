2821267eea978e35da832bcbad52cb54cb001005
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-26 06:51:28 +0900

    Format Property Data

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,8 +1,41 @@
 "use server";
 
 async function addProperty(formData) {
-  console.log("add prperty action");
-  console.log(formData.get("name"));
+  // Access all values from amenities and images
+  const amenities = formData.getAll("amenities");
+  const images = formData
+    .getAll("images")
+    .filter((image) => image.name != "")
+    .map((image) => image.name);
+
+  const propertyData = {
+    type: formData.get("type"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    location: {
+      street: formData.get("location.street"),
+      city: formData.get("location.city"),
+      state: formData.get("location.state"),
+      zipcode: formData.get("location.zipcode"),
+    },
+    beds: formData.get("beds"),
+    baths: formData.get("baths"),
+    square_feet: formData.get("square_feet"),
+    amenities,
+    rates: {
+      weekly: formData.get("rates.weekly"),
+      monthly: formData.get("rates.monthly"),
+      nightly: formData.get("rates.nightly"),
+    },
+    seller_info: {
+      name: formData.get("seller_info.name"),
+      email: formData.get("seller_info.email"),
+      phone: formData.get("seller_info.phone"),
+    },
+    images,
+    //owner: userId,
+  };
+  console.log(propertyData);
 }
 
 export default addProperty;
