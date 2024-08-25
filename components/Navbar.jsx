2302f0ca99b6feed0a3efa5bc4879a9523f885a8
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-25 09:31:46 +0900

    Sign Out Button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -203,6 +203,10 @@ const Navbar = () => {
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
