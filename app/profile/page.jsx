a1bcc809a3d1a7eb6d3f2e75583e71caa08a5d57
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-26 09:36:52 +0900

    User Profile Listings

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,6 +3,7 @@ import profileDefault from "@/assets/images/profile.png";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
+import ProfileProperties from "@/components/ProfileProperties";
 
 const Profile = async () => {
   await connectDB();
@@ -14,6 +15,8 @@ const Profile = async () => {
   if (!userId) {
     throw new Error("userId is required");
   }
+
+  const properties = await Property.find({ owner: userId }).lean();
 
   return (
     <section className="bg-blue-50">
@@ -44,60 +47,7 @@ const Profile = async () => {
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/a1.jpg"
-                    alt="Property 1"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 1</p>
-                  <p className="text-gray-600">Address: 123 Main St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/b1.jpg"
-                    alt="Property 2"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 2</p>
-                  <p className="text-gray-600">Address: 456 Elm St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+              <ProfileProperties properties={properties} />
             </div>
           </div>
         </div>
