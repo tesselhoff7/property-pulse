c439dbcec02e3f57698ff1b2209d0f9e70ef0af2
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-29 08:25:05 +0900

    Saved Properties Page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import logo from "@/assets/images/logo-white.png";
 import profileDefault from "@/assets/images/profile.png";
 import { FaGoogle } from "react-icons/fa";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-//import UnreadMessageCount from "./UnreadMessageCount";
 
 const Navbar = () => {
   const { data: session } = useSession();
@@ -26,15 +25,7 @@ const Navbar = () => {
     };
 
     setAuthProviders();
-
-    // NOTE: close mobile menu if the viewport size is changed
-    window.addEventListener("resize", () => {
-      setIsMobileMenuOpen(false);
-    });
   }, []);
-
-  // NOTE: the aria-expanded attribute value should change with state for
-  // correct a11y
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -47,7 +38,7 @@ const Navbar = () => {
               id="mobile-dropdown-button"
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
-              aria-expanded={isMobileMenuOpen}
+              aria-expanded="false"
               onClick={() => setIsMobileMenuOpen((prev) => !prev)}
             >
               <span className="absolute -inset-0.5"></span>
@@ -118,8 +109,8 @@ const Navbar = () => {
                 {providers &&
                   Object.values(providers).map((provider, index) => (
                     <button
-                      onClick={() => signIn(provider.id)}
                       key={index}
+                      onClick={() => signIn(provider.id)}
                       className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                     >
                       <FaGoogle className="text-white mr-2" />
@@ -155,7 +146,10 @@ const Navbar = () => {
                     />
                   </svg>
                 </button>
-                {/*<UnreadMessageCount />*/}
+                <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+                  2
+                  {/* <!-- Replace with the actual number of notifications --> */}
+                </span>
               </Link>
               {/* <!-- Profile dropdown button --> */}
               <div className="relative ml-3">
@@ -164,7 +158,7 @@ const Navbar = () => {
                     type="button"
                     className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     id="user-menu-button"
-                    aria-expanded={isProfileMenuOpen}
+                    aria-expanded="false"
                     aria-haspopup="true"
                     onClick={() => setIsProfileMenuOpen((prev) => !prev)}
                   >
@@ -173,9 +167,9 @@ const Navbar = () => {
                     <Image
                       className="h-8 w-8 rounded-full"
                       src={profileImage || profileDefault}
-                      alt=""
                       width={40}
                       height={40}
+                      alt=""
                     />
                   </button>
                 </div>
@@ -196,9 +190,6 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
-                      onClick={() => {
-                        setIsProfileMenuOpen(false);
-                      }}
                     >
                       Your Profile
                     </Link>
@@ -208,9 +199,6 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
-                      onClick={() => {
-                        setIsProfileMenuOpen(false);
-                      }}
                     >
                       Saved Properties
                     </Link>
@@ -264,22 +252,17 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-
-            {!session &&
-              providers &&
-              Object.values(providers).map((provider, index) => (
-                <button
-                  onClick={() => signIn(provider.id)}
-                  key={index}
-                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
-                  <span>Login or Register</span>
-                </button>
-              ))}
+            {!session && (
+              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
+                <i className="fa-brands fa-google mr-2"></i>
+                <span>Login or Register</span>
+              </button>
+            )}
           </div>
         </div>
       )}
     </nav>
   );
 };
+
 export default Navbar;
