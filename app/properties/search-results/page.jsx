f23711aeb9e71f8c6f874ec616d6917016582d47
tesselhoff7 <tesselhoff7@gmail.com>
2024-08-30 08:35:22 +0900

    Display Search Results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -44,7 +44,7 @@ const SearchResultsPage = async ({
   return (
     <>
       <section className="bg-blue-700 py-4">
-        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8 ">
           <PropertySearchForm />
         </div>
       </section>
